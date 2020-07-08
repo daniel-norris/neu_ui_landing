52219063784b29c9243a4e15d0666f60a974b454
Daniel Norris <dan.norris@hotmail.com>
2020-07-08 01:13:32 +0100

    Updates

---
+++ precache-manifest.489f317df777a79d5732f9bc113b7fca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b65a90490d8f870f38275f2cba1dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "869bb19df8ae017952cb",
+    "url": "/static/css/main.9aa380d9.chunk.css"
+  },
+  {
+    "revision": "a27fa244219e4f584f09",
+    "url": "/static/js/2.90a39709.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.90a39709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869bb19df8ae017952cb",
+    "url": "/static/js/main.4234a669.chunk.js"
+  },
+  {
+    "revision": "bff128f7c374c2ffea9d",
+    "url": "/static/js/runtime-main.1c3f66be.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
